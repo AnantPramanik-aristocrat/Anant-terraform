5c93aef8e994dcffa78be11bd277b2e20e711d7f
AnantPramanik-aristocrat <116079840+AnantPramanik-aristocrat@users.noreply.github.com>
2025-09-03 06:30:33 +0000

    commit

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -2,3 +2,8 @@ output "public_IP" {
   value = aws_instance.example.*.public_ip
 
 }
+
+resource "local_file" "Inventory" {
+   content  = jsonencode(aws_instance.example.*.public_ip)
+   filename = "${path.module}/Ansible/Inventory.txt"
+}
