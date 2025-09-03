3180e249bf963746f62caa1e0c982fe0ba6fac28
AnantPramanik-aristocrat <116079840+AnantPramanik-aristocrat@users.noreply.github.com>
2025-09-03 04:41:00 +0000

    commit

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -19,7 +19,7 @@ terraform {
 
 provider "aws" {
     region = var.region
-    access_key = ${{ secrets.AWS_ACCESS_KEY}}
-    secret_key = ${{ secrets.AWS_SECRET_KEY}}
+    access_key = ${{secrets.AWS_ACCESS_KEY}}
+    secret_key = ${{secrets.AWS_SECRET_KEY}}
 }
 
