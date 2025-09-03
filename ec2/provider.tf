5f929d29acd7959936c0e52a50dd641591ce05b3
AnantPramanik-aristocrat <116079840+AnantPramanik-aristocrat@users.noreply.github.com>
2025-09-03 04:37:35 +0000

    commit

---
+++ ec2/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.10.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "anant-terraform-state4"
+    key     = "dev-env/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+    acl     = "private"
+    # make sure dynamodb table is present 
+    dynamodb_table = "anant-terraform-table4"
+  }
+}
+
+
+provider "aws" {
+    region = var.region
+      
+}
+
